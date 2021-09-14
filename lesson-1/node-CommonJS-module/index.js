995e0ceccead83cc2aee2f0c5b4b90e0c2308b2d
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-09-15 00:32:06 +0300

    finish lesson 1

---
+++ lesson-1/node-CommonJS-module/index.js
@@ -0,0 +1,26 @@
+// CommonJS
+// const nodemon = require("nodemon");
+
+const users = require("./users");
+// console.log(users);
+
+const {admins} = require("./users");
+// console.log(admins);
+
+// const {getCurrentMonth} = require("./date");
+
+// const currentMonth = getCurrentMonth();
+
+const currentMonth = require("./date").getCurrentMonth();
+// const currentMonth = require("./date/getCurrentMonth")();
+
+// const user = new require("./user");
+
+console.log(`Сейчас ${currentMonth} месяц`);
+
+
+
+
+
+
+
---
+++ lesson-1/npm-example/index.js
@@ -0,0 +1,1 @@
+console.log("Greate teacher Onidzuka!")
