c0f617d8b3f322b24350e7e4cba389ace5f6f18c
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-09-29 08:39:13 +0300

    finish lesson 4

---
+++ lesson-5/mongodb-project/app.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const {DB_HOST} = process.env;
+
+mongoose.connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("Database connect success")
+})
+.catch(error => {
+    console.log(error.message);
+})
